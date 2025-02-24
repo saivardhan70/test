84490558a4d748380d08fca586b99a92b56e0846
saivardhan70 <saivardhan70@gmail.com>
2025-02-24 20:02:25 +0530

    initialcommit

---
+++ tests/githublogin.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+
+test.beforeEach(async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  // Now you can perform your test actions on the page
+});
+
+test('test', async ({ page }) => {
+  await page.goto('https://github.com/');
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).fill('test');
+  await page.getByRole('textbox', { name: 'Password' }).fill('vardhan');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+});
